ddc2eaf1d8407094b865f53f45ae247623a8b4ff
Wendy Yunuen Arévalo Espinal <wendy@pickupon.io>
2023-12-26 12:38:47 -0600

    Locust test

---
+++ locustfile.py
@@ -0,0 +1,8 @@
+import time
+from locust import HttpUser, task
+
+#locust --headless --host "http://127.0.0.1:5000" -u 1 --run-time 180 --csv=results
+class QuickstartUser(HttpUser):
+    @task
+    def numerical_integration(self):
+        self.client.get("/numericalintegralservice/0/3.14159")
